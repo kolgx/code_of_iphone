3f736dc6d9c00d29d4a62cb30020831fde6d8306
kolgx <2547525908@qq.com>
2021-06-29 16:16:51 +0800

    orange

---
+++ app/src/main/java/cn/edu/neusoft/lgx/shope_of_iphone/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.neusoft.lgx.shope_of_iphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
